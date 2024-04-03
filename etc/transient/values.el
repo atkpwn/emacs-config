bca05045a7718bd005e41d6b6cdd3ed976dd7586
Attakorn Putwattana <atkpwn@gmail.com>
2024-04-04 00:26:03 +0200

    make magit-fetch prune

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,1 +1,2 @@
-((magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature"))
+((magit-fetch "--prune")
+ (magit-log:magit-log-mode "-n256" "--graph" "--decorate" "--show-signature"))
