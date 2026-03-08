d99af489afea3b1101622880b3765115cbc3bb59
Attakorn Putwattana <atkpwn@gmail.com>
2026-03-08 13:24:11 +0100

    fix org-config load-path

--- init.el
+++ init.el
@@ -1050,7 +1050,8 @@ targets."
   (terraform-mode . eglot-ensure)
   (terraform-mode . outline-minor-mode))
 
-(use-package org-config)
+(use-package org-config
+  :load-path "lisp")
 
 (use-package citar
   :bind
